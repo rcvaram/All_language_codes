2bf4b4dee3554deae6a42a765313e43af6583f0a
Ricardo Monascal <rmonascal@gmail.com>
2019-10-24 16:37:12 -0400

    Add Hello World program in Haskell

---
+++ Haskell/hello_world.hs
@@ -0,0 +1,8 @@
+{- hello_world.hs:
+ - ---------------
+ -    This program is the famous (or infamous) "Hello World!" implemented in
+ - the functional programming lanugage: Haskell.
+ -}
+
+main :: IO ()
+main = putStrLn "Hello World!"
