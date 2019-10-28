7aceda11d1a38e7c6d868f4bfd78033fb56d34fb
Divay Garg <divay.garg.work@gmail.com>
2019-10-28 23:57:54 +0530

    added binary search in C

---
+++ C/binary_search.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+
+// Recursive method to search
+//Input  : Sorted Array, start position, end position, number to be found
+//output : Index of the number 
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+    while (l <= r) { 
+        int m = l + (r - l) / 2; 
+  
+        // Check if x is present at mid 
+        if (arr[m] == x) 
+            return m; 
+  
+        // If x greater, ignore left half 
+        if (arr[m] < x) 
+            l = m + 1; 
+  
+        // If x is smaller, ignore right half 
+        else
+            r = m - 1; 
+    } 
+  
+    // if element is not present return -1 
+    return -1; 
+} 
