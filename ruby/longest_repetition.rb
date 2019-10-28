190ef7d530e571ccdb530e125870b98a08f7e81f
Henrique <hguedesbezerra@gmail.com>
2019-10-27 20:35:08 -0400

    Added longest repetition in ruby

---
+++ ruby/longest_repetition.rb
@@ -0,0 +1,8 @@
+def longest_repetition(string)
+    max = string
+            .chars
+            .chunk(&:itself)
+            .map(&:last)
+            .max_by(&:size)
+    max ? [max[0], max.size] : ["", 0]
+end
