63c799d44514699e5b791c327a646cf9d568395c
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2019-10-30 09:41:51 -0300

    Create Sum.java file

---
+++ Java/Sum.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Sum{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Sum = " + scanner.nextInt() + scanner.nextInt());
+	}// End main()
+}// End Sum
