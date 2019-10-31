fdbb6030bec6a99e48ffffa33410669d5e638e21
Lucas Cavalcante <co.lucasc@gmail.com>
2019-10-31 15:56:51 -0300

    Add custom delegate example with Kotlin

--- C/helloworld.c
+++ C/helloworld.c
@@ -1,5 +1,7 @@
-i#include <stdio.h>
-
-int main{
-  printf("Hello World"):
+#include <stdio.h>
+void main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   //return 0;
 }
